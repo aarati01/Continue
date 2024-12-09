3f82a273ee1c647fe99080b72343097f02e32a4d
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-09 11:06:33 -0500

    array list is added in the usestate

--- src/assets/App2.jsx
+++ src/assets/App2.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
+
 const App2 = () => {
-  const [showbtn,setshowbtn]=useState(false)
+  const [showbtn, setshowbtn] = useState(false);
+  const [todos] = useState([
+    {
+      title: "hey",
+      desc: "I am a good todo"
+    },
+    {
+      title: "Hey another todo",
+      desc: "I am a good todo tooo"
+    },
+    {
+      title: "hey i am grocery todo",
+      desc: "I am a good todo"
+    }
+  ]);
+
   return (
     <>
-    {showbtn?<button>Showbtn is true</button>:<button>showbtn is false</button>}
+      {/* {showbtn?<button>Showbtn is true</button>:<button>showbtn is false</button>} */}
 
-    {/* {showbtn && <button>showbtn is true</button>} */}
-    <div>
-      <button onClick={()=>setshowbtn(!showbtn)}>Toogle showbtn</button>
-      
-    </div>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.title}>
+            <div className="todo">{todo.title}</div>
+            <div className="todo">{todo.desc}</div>
+          </div>
+        );
+      })}
+
+      {showbtn && <button>showbtn is true</button>}
+      <div>
+        <button onClick={() => setshowbtn(!showbtn)}>Toogle showbtn</button>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default App2
+export default App2;
